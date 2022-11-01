d3bf7e9bbbb68c411809782699af47636c1aa85d
trevorhudson <trevor.c.hudson@gmail.com>
2022-11-01 12:26:50 -0700

    Update docstring

--- step1.js
+++ step1.js
@@ -2,21 +2,24 @@
 
 const fsP = require('fs/promises');
 
-/** cat: read file with path and print contents of file */
-
+/** cat: read file with path and print contents of file
+ *
+ * - Input: filepath
+ * - Output: logs to console
+ * - on Error: returns error message and exits process
+*/
 async function cat(path) {
   try {
     const content = await fsP.readFile(path, "utf8");
     console.log(content);
   }
-  catch(error) {
+  catch (error) {
     console.error(`
-      Error reading ${path}:
-        Error: ENOENT: no such file or directory, open '${path}'
-    `);
+      Error reading ${path}: ${error}`);
     process.exit(1);
   }
 }
 
-cat("./one.txt");
-cat("blah");
+cat(process.argv[2])
+
+
