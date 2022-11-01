e4f535a48137695186695923c4a39745d79330cc
trevorhudson <trevor.c.hudson@gmail.com>
2022-11-01 12:27:18 -0700

    Added webCat() function, and handler function main()

---
+++ step2.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const fsP = require('fs/promises');
+const axios = require('axios');
+
+/** cat: read file with path and print contents of file to console.
+ *
+ * - Input: filepath
+ * - Output: logs to console
+ * - on Error: returns error message and exits process
+*/
+
+async function cat(path) {
+  try {
+    const content = await fsP.readFile(path, "utf8");
+    console.log(content);
+  }
+  catch (error) {
+    console.error(`
+      Error reading ${path}:
+        Error: ENOENT: no such file or directory, open '${path}'
+    `);
+    process.exit(1);
+  }
+}
+
+
+/** webcat: reads contents of URL and prints to contents to the console.
+ *
+ * - Input: URL
+ * - Output: logs to console
+ * - on Error: returns error message and exits process
+ *
+*/
+
+async function webCat(url) {
+
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+  }
+  catch (error) {
+    console.error(`Error fetching ${url}:
+    Error: Request failed with status code 404`);
+  }
+}
+
+/** Handler function for process arguments.
+ * If path is URL, calls webCat()
+ * Else if filepath, calls cat()
+ */
+function main(path) {
+
+  if (path.includes('http')) {
+    webCat(path);
+  } else {
+    cat(path);
+  }
+
+}
+
+const path = process.argv[2];
+main(path);
+
