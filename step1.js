8a14fcdee611277c5d5b5a8962d2a5ae8b314750
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-01 11:56:58 -0700

    Complete step1

---
+++ step1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { CallTracker } = require('assert');
+const fsP = require('fs/promises');
+
+/** cat: read file with path and print contents of file */
+
+async function cat(path) {
+  try {
+    const content = await fsP.readFile(path, "utf8");
+    console.log(content);
+  }
+  catch(error) {
+    console.error(`
+      Error reading ${path}:
+        Error: ENOENT: no such file or directory, open '${path}'
+    `);
+    process.exit(1);
+  }
+}
+
+cat("./one.txt");
+cat("blah");
